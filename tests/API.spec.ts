4c85fac50da67c4aaf2fa8deb7cd73213bafc541
Rafi Shaik <rafi.tgcs219@gmail.com>
2026-09-23 11:57:28 +0530

    added create order API

--- tests/API.spec.ts
+++ tests/API.spec.ts
@@ -1,6 +1,7 @@
 import {test,expect,request} from '@playwright/test';
 
 const loginPayLoad = {userEmail: "rafi.tgcs219@gmail.com", userPassword: "MBxanptFWYri5$k"};
+const createOrderPayLoad = {orders: [{country: "India", productOrderedId: "6960eae1c941646b7a8b3ed3"}]}
 let token:string;
 
 test.beforeAll( async ()=>{
@@ -10,6 +11,19 @@ test.beforeAll( async ()=>{
     const apiResJson=await apiRes.json();
     token=await apiResJson.token;
     console.log(token);
+
+    //create order
+    const apiContext2 = await request.newContext();
+    const apiRes2= await apiContext2.post("https://rahulshettyacademy.com/api/ecom/order/create-order",{
+        data : createOrderPayLoad,
+        headers :{
+            authorization : token,
+            'Content-Type':'application/json',
+        }
+    })
+    const apiResJson2=await apiRes2.json();
+    const orderid = await apiResJson2.orders[0];
+    console.log(orderid);
 
 });
 
@@ -22,5 +36,11 @@ test('API Login TestCase', async ({page})=>{
 
     await page.goto("https://rahulshettyacademy.com/client");
     await expect(page).toHaveTitle("Let's Shop");
+
+    await page.getByRole("button",{name:'  ORDERS'}).click();
+
+    const orderstitle=await page.locator("text='Automation Practice'").textContent();
+    console.log(orderstitle)
+    await expect(page.locator("text='Automation Practice'")).toHaveText("Automation Practice");
 
 });
