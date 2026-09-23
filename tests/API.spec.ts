a6852a40f063fab33bf7fc2aafc873c79ea4db89
Rafi Shaik <rafi.tgcs219@gmail.com>
2026-09-23 12:16:04 +0530

    added mocl network intercept

--- tests/API.spec.ts
+++ tests/API.spec.ts
@@ -2,6 +2,7 @@ import {test,expect,request} from '@playwright/test';
 
 const loginPayLoad = {userEmail: "rafi.tgcs219@gmail.com", userPassword: "MBxanptFWYri5$k"};
 const createOrderPayLoad = {orders: [{country: "India", productOrderedId: "6960eae1c941646b7a8b3ed3"}]}
+const fakePayLoad={"data":[],"message":"No Orders"};
 let token:string;
 
 test.beforeAll( async ()=>{
@@ -37,10 +38,25 @@ test('API Login TestCase', async ({page})=>{
     await page.goto("https://rahulshettyacademy.com/client");
     await expect(page).toHaveTitle("Let's Shop");
 
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/6a48b09085b8849b49c88b7f",async route =>{
+
+        
+        //const response= await page.request.fetch(route.request());
+        const body = JSON.stringify(fakePayLoad);
+
+        await route.fulfill({
+             //response,
+             body,
+        });
+    })
+
     await page.getByRole("button",{name:'  ORDERS'}).click();
 
     const orderstitle=await page.locator("text='Automation Practice'").textContent();
     console.log(orderstitle)
     await expect(page.locator("text='Automation Practice'")).toHaveText("Automation Practice");
+
+    await page.pause();
+    
 
 });
